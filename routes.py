48ce229bee98009b0be2b42b93ddc23807145164
orotib <oros.tibor0@gmail.com>
2016-11-06 14:45:21 +0100

    Added home.html and home route to routes.py

--- routes.py
+++ routes.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, request
-from models import db
+from flask import Flask, render_template, request, session, redirect, url_for
+from models import db, User
 from forms import SignupForm
 
+#import mysql.connector
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://localhost/test'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:pass@localhost/test'
 db.init_app(app)
 
 app.secret_key = 'development-key'
@@ -24,10 +26,19 @@ def signup():
 		if form.validate() == False:
 			return render_template('signup.html', form=form)
 		else:
-			return 'Success!'
+			newuser = User(form.first_name.data, form.last_name.data, form.email.data, form.password.data)
+			db.session.add(newuser)
+			db.session.commit()
+
+			session['email'] = newuser.email
+			return redirect(url_for('home'))
 	elif request.method == 'GET':
 		return render_template('signup.html', form=form)
 
+
+@app.route('/home')
+def home():
+	return render_template('home.html')
 ########################
 
 if __name__ == '__main__':
