2841d75b79fcd2ae6dcbc138379237cd5da75084
orotib <oros.tibor0@gmail.com>
2016-11-06 14:18:20 +0100

    Updating files.

--- routes.py
+++ routes.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from models import db
+from forms import SignupForm
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://localhost/test'
+db.init_app(app)
+
+app.secret_key = 'development-key'
 
 @app.route('/')
 def index():
@@ -9,6 +15,18 @@ def index():
 @app.route('/about')
 def about():
 	return render_template('about.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+	form = SignupForm()
+
+	if request.method == 'POST':
+		if form.validate() == False:
+			return render_template('signup.html', form=form)
+		else:
+			return 'Success!'
+	elif request.method == 'GET':
+		return render_template('signup.html', form=form)
 
 ########################
 
