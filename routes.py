c88695ce62b01cbf1cb5054635a0871a338c55ff
orotib <oros.tibor0@gmail.com>
2016-11-11 15:00:15 +0100

    Refreshed routes/forms.py and index.html and added login.html' ;

--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 from models import db, User
-from forms import SignupForm
+from forms import SignupForm, LoginForm
 
 #import mysql.connector
 
@@ -31,13 +31,38 @@ def signup():
 			db.session.commit()
 
 			session['email'] = newuser.email
-			return redirect(url_for('home'))
+			return redirect(url_for('login'))
 	elif request.method == 'GET':
 		return render_template('signup.html', form=form)
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if request.method == 'POST':
+		if form.validate() == False:
+			return render_template('login.html', form=form)
+		else:
+			email = form.email.data
+			password = form.password.data
+		user = User.query.filter_by(email=email).first()
+		if user is not None and user.check_password(password):
+			session['email'] = form.email.data
+			return redirect(url_for('home'))
+		else:
+			return redirect(url_for('login'))
+	elif request.method == 'GET':
+		return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout():
+	session.pop('email', None)
+	return redirect(url_for('index'))
 
 @app.route('/home')
 def home():
+	if 'email' not in session:
+		return redirect(url_for('login'))
+	
 	return render_template('home.html')
 ########################
 
